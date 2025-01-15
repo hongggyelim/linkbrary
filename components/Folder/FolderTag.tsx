13e8446176a7d2ece58750bbaada2a0fe0245537
hongggy <hongggy@gmail.com>
2025-01-15 13:57:16 +0900

    chore: 전체 버튼에 색상 추가

--- components/Folder/FolderTag.tsx
+++ components/Folder/FolderTag.tsx
@@ -7,25 +7,29 @@ const FolderTag = ({ folderList }: FolderListData) => {
 
   const folderStyle =
     "w-[100px] h-[35px] px-[15px] text-sm whitespace-nowrap truncate border border-purple100 rounded-md hover:bg-purple100 hover:text-white";
-
+  const selectedStyle = "bg-purple100 text-white";
   const handleSubmit = (selectedFolderId: number | string) => {
     router.push({
       pathname: router.pathname,
       query: selectedFolderId ? { folder: selectedFolderId } : {},
     });
   };
+  const isAllSelected = currentFolderId === "" || currentFolderId === undefined;
 
   return (
     <ul className="flex flex-wrap gap-[8px] lg:w-[80%] md:w-[80%]">
       <li>
-        <button className={folderStyle} onClick={() => handleSubmit("")}>
+        <button
+          className={`${folderStyle} ${isAllSelected && selectedStyle}`}
+          onClick={() => handleSubmit("")}
+        >
           전체
         </button>
       </li>
       {folderList.slice(0, 8).map((folder) => (
         <li key={folder.id}>
           <button
-            className={`${folderStyle} ${folder.id === Number(currentFolderId) && "bg-purple100 text-white"}`}
+            className={`${folderStyle} ${folder.id === Number(currentFolderId) && selectedStyle}`}
             type="submit"
             onClick={() => handleSubmit(folder.id)}
           >
