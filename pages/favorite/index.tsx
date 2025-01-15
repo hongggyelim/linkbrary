2eaa2a8a673080aaf7fb574f6280fdc5e206825e
hongggy <hongggy@gmail.com>
2025-01-15 13:37:49 +0900

    fix: 스켈레톤 반응형 개수 수정

--- pages/favorite/index.tsx
+++ pages/favorite/index.tsx
@@ -87,7 +87,7 @@ const FavoritePage = ({
         </div>
         {/* 로딩 중일 때 */}
         {isLoading ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {[...Array(3)].map((_, index) => (
               <LinkCardSkeleton key={index} />
             ))}
--- pages/link/index.tsx
+++ pages/link/index.tsx
@@ -117,7 +117,7 @@ const LinkPage = ({
             )}
           </div>
           {isLoading ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {[...Array(3)].map((_, index) => (
                 <LinkCardSkeleton key={index} />
               ))}
