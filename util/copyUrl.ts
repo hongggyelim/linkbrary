64c2b5f03a03c0e35da3b617db96d6e17476ab67
hongggy <hongggy@gmail.com>
2025-01-21 13:05:40 +0900

    fix: 링크 복사 주소 수정

--- util/copyUrl.ts
+++ util/copyUrl.ts
@@ -5,7 +5,7 @@ export const handleCopyUrl = () => {
   // 현재 URL에서 folderId를 추출
   const url = new URL(location.href);
   const folderId = url.searchParams.get("folder");
-  const shareUrl = `https://linkbrary-redploy.vercel.app/share/${folderId}`;
+  const shareUrl = `https://linkbrary-redeploy.vercel.app/share/${folderId}`;
   navigator.clipboard.writeText(shareUrl);
   toast.success(toastMessages.success.copyLink);
 };
