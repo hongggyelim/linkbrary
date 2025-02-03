4c12001d1828ce9dce7c56166e944fd661b85d14
hongggy <hongggy@gmail.com>
2025-02-03 16:14:32 +0900

    fix: empty 컴포넌트 정렬 수정

--- components/Link/RenderEmptyLinkMessage.tsx
+++ components/Link/RenderEmptyLinkMessage.tsx
@@ -2,14 +2,12 @@ import { AiTwotoneSmile } from "react-icons/ai";
 
 const RenderEmptyLinkMessage = () => {
   return (
-    <>
-      <span className="w-full h-[200px] flex justify-center items-center">
-        저장된 링크가 없습니다.
-      </span>
-      <span>
+    <div className="flex-col w-full flex justify-center items-center">
+      <span className="">저장된 링크가 없습니다.</span>
+      <span className="flex items-center gap-1">
         먼저 폴더를 추가해보세요 <AiTwotoneSmile />
       </span>
-    </>
+    </div>
   );
 };
 
