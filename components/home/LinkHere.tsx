a4743305dc68ca73d2e115b6045af8dee991c536
hongggy <hongggy@gmail.com>
2025-02-03 16:55:31 +0900

    chore: 로그인 전 보이는 문구 추가

--- components/home/LinkHere.tsx
+++ components/home/LinkHere.tsx
@@ -1,8 +1,9 @@
-import { PiHandPalmDuotone } from "react-icons/pi";
+import { PiHandPalmDuotone, PiFolderSimpleStarDuotone } from "react-icons/pi";
 
 const LinkHere = () => {
+  const align = "flex justify-center items-center";
   return (
-    <div className="text-[24px] font-semibold text-orange50 my-10">
+    <div className="text-[24px] font-semibold text-orange50 mt-[80px] flex flex-col justify-center">
       <p>
         다음에 보려던 &#32;
         <span className="text-orange100 font-bold">&#32;링크&#32;</span>
@@ -11,6 +12,14 @@ const LinkHere = () => {
       <p className="flex justify-center items-center">
         <span>여기 있어요&#32;</span>
         <PiHandPalmDuotone />
+      </p>
+      <br />
+      <p className="flex justify-center items-center flex-col">
+        <span className="flex items-center gap-1">
+          <PiFolderSimpleStarDuotone />
+          <span>폴더로 관리하고</span>
+        </span>
+        <span>친구에게 공유하세요</span>
       </p>
     </div>
   );
