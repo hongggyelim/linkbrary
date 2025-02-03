d29b8abae38fef0648a33ee86b5da5f4404da783
hongggy <hongggy@gmail.com>
2025-02-03 16:03:07 +0900

    chore: 썸네일 변경

--- components/home/LinkHere.tsx
+++ components/home/LinkHere.tsx
@@ -2,10 +2,10 @@ import { PiHandPalmDuotone } from "react-icons/pi";
 
 const LinkHere = () => {
   return (
-    <div className="text-[24px] font-semibold text-orange50">
+    <div className="text-[24px] font-semibold text-orange50 my-10">
       <p>
         다음에 보려던 &#32;
-        <span className="text-orange100 font-bold">링크&#32;</span>
+        <span className="text-orange100 font-bold">&#32;링크&#32;</span>
         어디있지?
       </p>
       <p className="flex justify-center items-center">
